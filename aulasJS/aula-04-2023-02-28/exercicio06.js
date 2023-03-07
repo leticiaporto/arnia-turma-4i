2f759f9e9135247d819707939d847dea2ccb2eb0
leticiaporto <leticiaportosoares@gmail.com>
2023-03-06 21:30:29 -0300

    update: aula 04 JS | 02/03/2023

--- aulasJS/aula-04-2023-02-28/exercicio06.js
+++ aulasJS/aula-04-2023-02-28/exercicio06.js
@@ -1,15 +1,33 @@
-let quantidade = 5
+/*
+A sequência de Fibonacci é a sequência de números naturais  onde os dois primeiros são 1 e o próximo é a soma dos dois anteriores. Faça um programa que imprime os 50 primeiros termos dessa sequência
+*/
 let contador = 0
 let elemento = 1
 let anterior = 0
-let anteriorTemporario
 
-while(contador < 5) {
-    console.log(elemento) // 1 - 1 - 2 - 3 - 5 - 8 
+while(contador < 50){ // 1ª - 2ª - 3ª - 4ª
+    console.log(elemento) // 1 - 1 - 2 - 3
 
-    anteriorTemporario = elemento // guarda o valor do elemento antes de recalcular
-    elemento = elemento + anterior // calcula o novo elemento da serie
-    anterior = anteriorTemporario // atualiza o anterior
+    let anteriorTemporario = elemento // 1 - 1 - 2 - 3
+    elemento = elemento + anterior // 1 - 2 - 3 - 5
+    anterior = anteriorTemporario // 1 - 1 - 2 - 3
 
     contador++
 }
+
+/* 
+    Solução alternativa do colega Igor
+
+    let num1 = 0
+    let num2 = 1
+    let contador = 0
+    let num3 = 0
+
+    while (contador < 50) {
+        io.write(num2)
+        num3 = num1 + num2
+        num1 = num2
+        num2 = num3
+        contador++
+    }
+*/
