2ff4c8fbdba7dd7b736de928996fe9346de41774
leticiaporto <leticiaportosoares@gmail.com>
2023-03-09 22:30:18 -0300

    update: aula 11 JS | 09/03/2023

--- aulasJS/aula-11-2023-02-09/exercicio06.js
+++ aulasJS/aula-11-2023-02-09/exercicio06.js
@@ -1,12 +1,22 @@
+const io = require('../io')
+
+let nomes = []
+let notas = []
+
 function ordenaDados(nomes, notas) {
+    let soma = 0
+    let media
+
     for (let i = 0; i < nomes.length - 1; i++) {
-        //este for leva o maior para última posição
+        //este for leva a maior letra para última posição
         for (let j = 0; j < nomes.length - 1; j++) {
-            if (nomes[j][0] > nomes[j + 1][0]) {
+            if (nomes[j][0] > nomes[j + 1][0]) { //acessa a primera letra dos nomes
+                // troca a posição dos nomes
                 let aux = nomes[j]
                 nomes[j] = nomes[j + 1]
                 nomes[j + 1] = aux
 
+                // troca a posição das notas para manter a "conexão" dos índices de nomes e notas
                 let aux2 = notas[j]
                 notas[j] = notas[j + 1]
                 notas[j + 1] = aux2
@@ -14,12 +24,25 @@ function ordenaDados(nomes, notas) {
         }
     }
 
-    for (let i = 0; i < nomes.length; i++) {
-        console.log('Nome:', nomes[i], 'Nota:', notas[i])
+    for (let i = 0; i < notas.length; i++) {
+        soma = soma + notas[i]
     }
 
+    media = soma / notas.length
+
+    for (let i = 0; i < nomes.length; i++) {
+        io.write('Nome: ' + nomes[i] + ' - Nota: ' + notas[i])
+    }
+
+    io.write('Média das Notas: ' + media)
 }
 
-const nomes = ['Letícia', 'Adolfo', 'Márcio', 'Grasi','Alex']
-const notas = [9, 9.5, 8, 9.8, 9.1]
+for (let i = 0; i < 5; i++) {
+    io.write('Digite um nome: ')
+    let nome = io.read()
+    nomes.push(nome)
+    io.write('Digite uma nota: ')
+    let nota = io.readFloat()
+    notas.push(nota)
+}
 ordenaDados(nomes, notas)
